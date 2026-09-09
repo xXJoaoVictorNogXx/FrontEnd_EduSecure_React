18b7a587023bed61e3b2a3e6f5c61167cdb1ce3b
SEMFAZSL\61135586306 <joaovictorfcnogueira@gmail.com>
2026-09-09 09:29:21 -0300

    Add react-router-dom and set up basic routing structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from "./routes.tsx";
+import { RouterProvider } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
---
+++ src/routes.tsx
@@ -0,0 +1,10 @@
+// src/routes.tsx
+import { createBrowserRouter } from "react-router-dom";
+// Importe outras telas aqui futuramente (ex: Login, Configuracoes)
+
+export const router = createBrowserRouter([
+  // {
+  //   path: "/login",
+  //   element: <Login />,
+  // }
+]);
